b0acd4110784caff09f72dc3e02370f64638d88c
webdevalgo <webdevalgo@gmail.com>
2025-07-06 10:11:12 +0500

    if verified, reduce files

--- src/verify.ts
+++ src/verify.ts
@@ -6,10 +6,11 @@ if (import.meta.main) {
   console.log();
   const dir = Array.from(Deno.readDirSync("data/")).filter((d) =>
     d.name.endsWith(".json")
-  ).sort((a, b) => b.name < a.name ? -1 : 0);
+  ).sort((a, b) => b.name < a.name ? 1 : -1);
   for (const file of dir) {
     if (!file.isFile) continue;
     if (!file.name.endsWith(".json")) continue;
+    if (file.name === "0.json") continue;
     const text = Deno.readTextFileSync(`data/${file.name}`);
     const json = JSON.parse(text);
     if (json.pool !== poolId) continue;
@@ -27,6 +28,23 @@ if (import.meta.main) {
         console.log();
         Deno.exit(1);
       }
+    }
+    if (file.name !== dir.at(-1)?.name) {
+      Deno.writeTextFileSync("data/_.jsonl", JSON.stringify(json) + "\n", {
+        append: true,
+        create: true,
+      });
+      Deno.removeSync(`data/${file.name}`);
+    }
+    try {
+      Deno.removeSync(`data/${file.name.replace(".json", ".csv")}`);
+    } catch (_) {
+      // Ignore if the CSV file does not exist
+    }
+    try {
+      Deno.removeSync(`data/${file.name.replace(".json", ".txns")}`);
+    } catch (_) {
+      // Ignore if the txns file does not exist
     }
   }
 }
